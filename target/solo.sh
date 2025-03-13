ec446d567cd02fce5961c23788e45c27ea6cd267
Daniele Ottaviano <daniele.ottaviano@tum.de>
2025-03-13 17:09:29 +0100

    Updated scripts to automate the experiments
    Signed-off-by: Daniele Ottaviano <daniele.ottaviano@tum.de>

---
+++ target/solo.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# Setup Board  Environment
+./setup_board.sh
+
+# Load Jailhouse
+cd ~
+./scripts_jailhouse_kria/jailhouse_setup/jailhouse_start.sh -o
+
+# Load the Oracle Cell
+jailhouse cell create jailhouse/configs/arm64/zynqmp-kv260-APU-inmate-demo.cell 
+jailhouse cell load inmate-demo-APU jailhouse/inmates/demos/arm64/oracle-demo.bin
+jailhouse cell start inmate-demo-APU
+
+# Run the Marte2 Application
+cd ~/tests/marte2_test
+./MARTeApp.sh -l RealTimeLoader -s State1 -f ./Configuration/VSES-Control.cfg
+
