8fae43a6812fd5c82724abea6c531f6be908614c
Daniele Ottaviano <dottavia@frodo.localhost>
2025-03-13 17:25:26 +0100

    Uploaded script to run all the experiments
    Signed-off-by: Daniele Ottaviano <dottavia@frodo.localhost>

---
+++ server_udp/all_vs_tests.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+./vs_test.sh solo
+
+./vs_test.sh membomb
+
+./vs_test.sh solo col
+
+./vs_test.sh membomb col
+
+./vs_test.sh solo bwreg
+
+./vs_test.sh membomb bwreg
+
+./vs_test.sh solo col bwreg
+
+./vs_test.sh membomb col bwreg
