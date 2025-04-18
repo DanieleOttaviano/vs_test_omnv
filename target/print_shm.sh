b23503d5b7e5b1e72aa4a57a68f20a6c83bfa08b
Daniele Ottaviano <dottavia@frodo.localhost>
2025-04-18 11:41:12 +0200

    adjusted print shm with actual float values
    Signed-off-by: Daniele Ottaviano <dottavia@frodo.localhost>

--- target/print_shm.sh
+++ target/print_shm.sh
@@ -2,30 +2,35 @@
 
 BASE_ADDR=0x46d00000
 
-# Helper function to read a 64-bit double from memory
-read_double() {
-    local addr=$1
-    value=$(devmem "$addr" 64)
-    printf "%.6f" "$value"
+# Convert a hex 64-bit number to IEEE 754 double using Python
+hex_to_double() {
+    local hexval=$1
+    python3 -c "import struct; print(struct.unpack('<d', struct.pack('<Q', int('$hexval', 16)))[0])"
 }
 
-# Helper function to read a 32-bit unsigned int
-read_uint32() {
-    local addr=$1
-    value=$(devmem "$addr" 32)
-    printf "0x%08X" "$value"
-}
-
-# Helper function to read a byte
-read_byte() {
-    local addr=$1
-    value=$(devmem "$addr" 8)
-    printf "0x%02X" "$value"
-}
-
-# Hex offset adder
+# Helper: Add hex offset
 hex_add() {
     printf "0x%X\n" $(( $1 + $2 ))
+}
+
+# Read a 64-bit value from memory and decode as double
+read_double() {
+    local addr=$1
+    raw=$(devmem "$addr" 64)
+    hex=$(printf "%016x\n" "$raw")
+    hex_to_double "$hex"
+}
+
+# Read a 32-bit value as uint32
+read_uint32() {
+    local addr=$1
+    devmem "$addr" 32
+}
+
+# Read an 8-bit value
+read_byte() {
+    local addr=$1
+    devmem "$addr" 8
 }
 
 # Read array of doubles
@@ -38,7 +43,8 @@ read_double_array() {
     for ((i=0; i<count; i++)); do
         offset=$((i * 8))
         addr=$(hex_add $start_addr $offset)
-        printf "  [%02d] @ %s = %s\n" $i "$addr" "$(read_double $addr)"
+        value=$(read_double $addr)
+        printf "  [%02d] @ %s = %.6f\n" $i "$addr" "$value"
     done
 }
 
@@ -48,22 +54,32 @@ echo "Reading shared_page structure from $BASE_ADDR"
 read_double_array $BASE_ADDR 11 "pf_voltage"
 
 # vs3 @ 0x058
-read_double $(hex_add $BASE_ADDR 0x058) | awk '{ print "vs3 =", $0 }'
+vs3_addr=$(hex_add $BASE_ADDR 0x058)
+vs3_val=$(read_double $vs3_addr)
+echo "vs3 @ $vs3_addr = $vs3_val"
 
 # pf_currents[11] @ 0x060
 read_double_array $(hex_add $BASE_ADDR 0x060) 11 "pf_currents"
 
 # ivs3 @ 0x0B8
-read_double $(hex_add $BASE_ADDR 0x0B8) | awk '{ print "ivs3 =", $0 }'
+ivs3_addr=$(hex_add $BASE_ADDR 0x0B8)
+ivs3_val=$(read_double $ivs3_addr)
+echo "ivs3 @ $ivs3_addr = $ivs3_val"
 
 # ip @ 0x0C0
-read_double $(hex_add $BASE_ADDR 0x0C0) | awk '{ print "ip =", $0 }'
+ip_addr=$(hex_add $BASE_ADDR 0x0C0)
+ip_val=$(read_double $ip_addr)
+echo "ip @ $ip_addr = $ip_val"
 
 # zc @ 0x0C8
-read_double $(hex_add $BASE_ADDR 0x0C8) | awk '{ print "zc =", $0 }'
+zc_addr=$(hex_add $BASE_ADDR 0x0C8)
+zc_val=$(read_double $zc_addr)
+echo "zc @ $zc_addr = $zc_val"
 
 # zdot @ 0x0D0
-read_double $(hex_add $BASE_ADDR 0x0D0) | awk '{ print "zdot =", $0 }'
+zdot_addr=$(hex_add $BASE_ADDR 0x0D0)
+zdot_val=$(read_double $zdot_addr)
+echo "zdot @ $zdot_addr = $zdot_val"
 
 # gaps[29] @ 0x0D8
 read_double_array $(hex_add $BASE_ADDR 0x0D8) 29 "gaps"
@@ -72,29 +88,32 @@ read_double_array $(hex_add $BASE_ADDR 0x0D8) 29 "gaps"
 read_double_array $(hex_add $BASE_ADDR 0x1C0) 2 "Xpoints"
 
 # Byte fields @ 0x1D0 - 0x1D3
-for offset in 0x1D0 0x1D1 0x1D2 0x1D3; do
-    label=""
-    case $offset in
-        0x1D0) label="oracle_decision" ;;
-        0x1D1) label="input_counter" ;;
-        0x1D2) label="marte_strategy" ;;
-        0x1D3) label="output_counter" ;;
+for i in {0..3}; do
+    addr=$(hex_add $BASE_ADDR $((0x1D0 + i)))
+    val=$(read_byte $addr)
+    case $i in
+        0) label="oracle_decision" ;;
+        1) label="input_counter" ;;
+        2) label="marte_strategy" ;;
+        3) label="output_counter" ;;
     esac
-    val=$(read_byte $(hex_add $BASE_ADDR $((0x${offset#0x}))))
-    echo "$label = $val"
+    echo "$label @ $addr = $val"
 done
 
 # reserved @ 0x1D4
-echo -n "reserved = "
-read_uint32 $(hex_add $BASE_ADDR 0x1D4)
-echo
+reserved_addr=$(hex_add $BASE_ADDR 0x1D4)
+reserved_val=$(read_uint32 $reserved_addr)
+echo "reserved @ $reserved_addr = $reserved_val"
 
 # es_output @ 0x1D8
-read_double $(hex_add $BASE_ADDR 0x1D8) | awk '{ print "es_output =", $0 }'
+es_output=$(read_double $(hex_add $BASE_ADDR 0x1D8))
+echo "es_output = $es_output"
 
 # classic_output @ 0x1E0
-read_double $(hex_add $BASE_ADDR 0x1E0) | awk '{ print "classic_output =", $0 }'
+classic_output=$(read_double $(hex_add $BASE_ADDR 0x1E0))
+echo "classic_output = $classic_output"
 
 # vs3_ref @ 0x1E8
-read_double $(hex_add $BASE_ADDR 0x1E8) | awk '{ print "vs3_ref =", $0 }'
+vs3_ref=$(read_double $(hex_add $BASE_ADDR 0x1E8))
+echo "vs3_ref = $vs3_ref"
 
